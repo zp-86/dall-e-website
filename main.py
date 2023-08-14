4aab0e5882733180471e90816b524b4a0cf9214b
zp-86 <99408147+zp-86@users.noreply.github.com>
2023-08-14 15:25:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import openai
+
+prompt = "test image"
+
+openai.api_key = "Api key here"
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index(img1=None, img2=None):
+    if "submit" in request.form:
+        prompt = request.form["prompt"]
+        response = openai.Image.create(
+            prompt=prompt,
+            n=2,
+            size="1024x1024"
+        )
+        img1 = response['data'][0]['url']
+        img2 = response['data'][1]['url']
+    return render_template('index.html', img1=img1, img2=img2)
+
+
+app.run(debug=True)
