e1e263fda0f86ec12ab9c91da32ad53451046538
zp86 <aplut2009@icloud.com>
2024-05-14 22:43:24 -0400

    Update to use new dall-e api and make public

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for, flash
-import openai
+from openai import OpenAI
 
-prompt = "test image"
-
-openai.api_key = "Api key here"
+client = OpenAI(
+    api_key=os.environ.get("API KEY HERE"),
+)
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index(img1=None, img2=None):
     if "submit" in request.form:
         prompt = request.form["prompt"]
-        response = openai.Image.create(
+        response = client.images.generate(
+            model="dall-e-2",
             prompt=prompt,
             n=2,
             size="1024x1024"
         )
-        img1 = response['data'][0]['url']
-        img2 = response['data'][1]['url']
+        img1 = response.data[0].url
+        img2 = response.data[1].url
     return render_template('index.html', img1=img1, img2=img2)
 
 
