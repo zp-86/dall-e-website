c33b0a5a6bf2caacaa081285750cd4c2859430a6
zp86 <aplut2009@icloud.com>
2024-07-01 01:04:38 -0400

    Uses dall-e-3

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for, flash
-from openai import OpenAI
+from openai_multi_client import OpenAIMultiClient
 
-client = OpenAI(
-    api_key=os.environ.get("API KEY HERE"),
-)
+api = OpenAIMultiClient(endpoint="images",
+                        data_template={"model": "dall-e-3"})
+
+
+
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index(img1=None, img2=None):
+def index(img1=None, img2=None, img=0):
     if "submit" in request.form:
         prompt = request.form["prompt"]
-        response = client.images.generate(
-            model="dall-e-2",
-            prompt=prompt,
-            n=2,
-            size="1024x1024"
-        )
-        img1 = response.data[0].url
-        img2 = response.data[1].url
+        def make_requests():
+            for num in range(1, 3):
+                api.request(data={
+                    "model": "dall-e-3", "prompt": prompt, "size": "1024x1024", "quality": "standard"
+                }, metadata={'num': num})
+        api.run_request_function(make_requests)
+        for result in api:
+            num = result.metadata['num']
+            response = result.response.data[0].url
+            img += 1
+            if img1 is None:
+                img1 = response
+            else:
+                img2 = response
+        img = 0
     return render_template('index.html', img1=img1, img2=img2)
 
 
-app.run()
+app.run(debug=True)
